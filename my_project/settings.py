1419c29e4ed354aacdd94292643d63ce0e223ea3
Nagad Bakal <codingbyher23@gmail.com>
2025-01-21 20:56:49 +0000

    Add herokuapp to allowed hosts

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@n=^ogck+hll$u+=32+-&6em27*@o0(-vs1+z5k#*gyxy9c9jx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG=False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000','.herokuapp.com']
 
 
 # Application definition
